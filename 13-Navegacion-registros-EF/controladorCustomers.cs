6ab252e665c1268fa7be905c403abc730bcc1d5f
profeCarballeira <carballeira2024@gmx.esluis@A30PC50>
2024-12-12 14:00:55 +0100

    13- modelos generados y controlador modificado

---
+++ 13-Navegacion-registros-EF/controladorCustomers.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using _13_Navegacion_registros_EF.Modelss;
+
+namespace _13_Navegacion_registros_EF
+{
+    public class CustomerController
+    {
+        private w3schoolsContext _context;
+
+        public CustomerController()
+        {
+            _context = new w3schoolsContext();
+        }
+
+        public List<Customer> GetAllCustomers()
+        {
+            return _context.Customers.ToList();
+        }
+
+        public void AddCustomer(Customer customer)
+        {
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+        }
+
+        public void UpdateCustomer(Customer customer)
+        {
+            var existingCustomer = _context.Customers.Find(customer.CustomerId);
+            if (existingCustomer != null)
+            {
+                // Mapear las propiedades actualizadas aquí
+                _context.SaveChanges();
+            }
+        }
+
+        public void DeleteCustomer(int CustomerId)
+        {
+            var customer = _context.Customers.Find(CustomerId);
+            if (customer != null)
+            {
+                _context.Customers.Remove(customer);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
