b8cf0052a631d0372522beb53b52b94e69e2c129
profeCarballeira <carballeira2024@gmx.esluis@A30PC50>
2024-12-12 14:37:13 +0100

    13 navegacion

--- 13-Navegacion-registros-EF/Form1.Designer.cs
+++ 13-Navegacion-registros-EF/Form1.Designer.cs
@@ -28,12 +28,121 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            bindingSource1 = new BindingSource(components);
+            label1 = new Label();
+            txtNombre = new TextBox();
+            txtDireccion = new TextBox();
+            label2 = new Label();
+            btnFirst = new Button();
+            btnPrevious = new Button();
+            btnNext = new Button();
+            btnLast = new Button();
+            ((System.ComponentModel.ISupportInitialize)bindingSource1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(248, 70);
+            label1.Name = "label1";
+            label1.Size = new Size(54, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Nombre:";
+            // 
+            // txtNombre
+            // 
+            txtNombre.Location = new Point(306, 62);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(321, 23);
+            txtNombre.TabIndex = 1;
+            // 
+            // txtDireccion
+            // 
+            txtDireccion.Location = new Point(306, 115);
+            txtDireccion.Name = "txtDireccion";
+            txtDireccion.Size = new Size(321, 23);
+            txtDireccion.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(240, 123);
+            label2.Name = "label2";
+            label2.Size = new Size(60, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Dirección:";
+            // 
+            // btnFirst
+            // 
+            btnFirst.Location = new Point(129, 333);
+            btnFirst.Name = "btnFirst";
+            btnFirst.Size = new Size(41, 33);
+            btnFirst.TabIndex = 4;
+            btnFirst.Text = "<<";
+            btnFirst.UseVisualStyleBackColor = true;
+            btnFirst.Click += btnFirst_Click;
+            // 
+            // btnPrevious
+            // 
+            btnPrevious.Location = new Point(189, 333);
+            btnPrevious.Name = "btnPrevious";
+            btnPrevious.Size = new Size(41, 33);
+            btnPrevious.TabIndex = 5;
+            btnPrevious.Text = "<";
+            btnPrevious.UseVisualStyleBackColor = true;
+            btnPrevious.Click += btnPrevious_Click;
+            // 
+            // btnNext
+            // 
+            btnNext.Location = new Point(279, 333);
+            btnNext.Name = "btnNext";
+            btnNext.Size = new Size(41, 33);
+            btnNext.TabIndex = 6;
+            btnNext.Text = ">";
+            btnNext.UseVisualStyleBackColor = true;
+            btnNext.Click += btnNext_Click;
+            // 
+            // btnLast
+            // 
+            btnLast.Location = new Point(338, 333);
+            btnLast.Name = "btnLast";
+            btnLast.Size = new Size(41, 33);
+            btnLast.TabIndex = 7;
+            btnLast.Text = ">>";
+            btnLast.UseVisualStyleBackColor = true;
+            btnLast.Click += btnLast_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnLast);
+            Controls.Add(btnNext);
+            Controls.Add(btnPrevious);
+            Controls.Add(btnFirst);
+            Controls.Add(txtDireccion);
+            Controls.Add(label2);
+            Controls.Add(txtNombre);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)bindingSource1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private BindingSource bindingSource1;
+        private Label label1;
+        private TextBox txtNombre;
+        private TextBox txtDireccion;
+        private Label label2;
+        private Button btnFirst;
+        private Button btnPrevious;
+        private Button btnNext;
+        private Button btnLast;
     }
 }
--- 13-Navegacion-registros-EF/Form1.cs
+++ 13-Navegacion-registros-EF/Form1.cs
@@ -1,10 +1,51 @@
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
 namespace _13_Navegacion_registros_EF
 {
     public partial class Form1 : Form
     {
+        private CustomerController _controller;
+
+        private BindingSource _bindingSource;
+
         public Form1()
         {
             InitializeComponent();
+            _controller = new CustomerController();
+            LoadData();
+            SetupDataBindings();
+        }
+        private void LoadData()
+        {
+            _bindingSource = new BindingSource();
+            _bindingSource.DataSource = _controller.GetAllCustomers();
+        }
+        private void SetupDataBindings()
+        {
+            //configurar los DataBindings para los TextBox
+            txtNombre.DataBindings.Add("Text", _bindingSource, "CustomerName");
+            txtDireccion.DataBindings.Add("Text", _bindingSource, "Address");
+        }
+
+
+        private void btnFirst_Click(object sender, EventArgs e)
+        {
+            _bindingSource.MoveFirst();
+        }
+
+        private void btnPrevious_Click(object sender, EventArgs e)
+        {
+            _bindingSource.MovePrevious();
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            _bindingSource.MoveNext();
+        }
+
+        private void btnLast_Click(object sender, EventArgs e)
+        {
+            _bindingSource.MoveLast();
         }
     }
 }
